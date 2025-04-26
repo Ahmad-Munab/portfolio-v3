01d5403b8baac0b0c2fd28181c56e344874df405
Ahmad Munab <ahmadmunab22@gmail.com>
2025-04-26 22:51:34 +0600

    added Raleway font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,14 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+import { Raleway } from "next/font/google";
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+const inconsolota = Raleway({
+  subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
   title: "Ahmad Munab || Portfolio",
-  description:
-    "Vortex | Co-Founder & COO @ZenDevz | Software Engineer | Business Enthusiast",
+  description: "Software Engineer | AI Enthusiast | SaaS Builder",
 };
 
 export default function RootLayout({
@@ -26,11 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <body className={`${inconsolota.className} antialiased`}>{children}</body>
     </html>
   );
 }
